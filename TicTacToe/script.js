5b48885540b96d947f6065aaa074d9fa7fd60417
Carlo Alain Aquino <carlo.aquino412@gmail.com>
2022-02-07 17:44:20 +0800

    added checkRowWin ability

--- TicTacToe/script.js
+++ TicTacToe/script.js
@@ -4,18 +4,17 @@ let board = [
     ["", "", ""]
 ]
 
-
-
 var square1 = document.getElementById("square1")
 square1.addEventListener("click", function(el){
     if(turnMove() % 2 === 0){
         square1.innerHTML = "X"
-        board[0].splice(0, 0, "X")
-    }else{
+        board[0].splice(0, 1, "X")
+    } else {
         square1.innerHTML = "O"
-        board[0].splice(0, 0, "O")
+        board[0].splice(0, 1, "O")
     }
     console.log(board)
+    checkRowWin()
 })
 
 var square2 = document.getElementById("square2")
@@ -23,11 +22,12 @@ square2.addEventListener("click", function(el){
     if(turnMove() % 2 === 0){
         square2.innerHTML = "X"
         board[0].splice(1, 1, "X")
-    }else{
+    } else {
         square2.innerHTML = "O"
         board[0].splice(1, 1, "O")
     }
     console.log(board)
+    checkRowWin()
 })
 
 var square3 = document.getElementById("square3")
@@ -35,23 +35,25 @@ square3.addEventListener("click", function(el){
     if(turnMove() % 2 === 0){
         square3.innerHTML = "X"
         board[0].splice(2, 2, "X")
-    }else{
+    } else {
         square3.innerHTML = "O"
         board[0].splice(2, 2, "O")
     }
     console.log(board)
+    checkRowWin()
 })
 
 var square4 = document.getElementById("square4")
 square4.addEventListener("click", function(el){
     if(turnMove() % 2 === 0){
         square4.innerHTML = "X"
-        board[1].splice(0, 0, "X")
-    }else{
+        board[1].splice(0, 1, "X")
+    } else {
         square4.innerHTML = "O"
-        board[1].splice(0, 0, "O")
+        board[1].splice(0, 1, "O")
     }
     console.log(board)
+    checkRowWin()
 })
 
 var square5 = document.getElementById("square5")
@@ -59,11 +61,12 @@ square5.addEventListener("click", function(el){
     if((turnMove() % 2 === 0)){
         square5.innerHTML = "X"
         board[1].splice(1, 1, "X")
-    }else{
+    } else {
         square5.innerHTML = "O"
         board[1].splice(1, 1, "O")
     }
     console.log(board)
+    checkRowWin()
 })
 
 var square6 = document.getElementById("square6")
@@ -71,23 +74,25 @@ square6.addEventListener("click", function(el){
     if((turnMove() % 2 === 0)){
         square6.innerHTML = "X"
         board[1].splice(2, 2, "X")
-    }else{
+    } else {
         square6.innerHTML = "O"
         board[1].splice(2, 2, "O")
     }
     console.log(board)
+    checkRowWin()
 })
 
 var square7 = document.getElementById("square7")
 square7.addEventListener("click", function(el){
     if((turnMove() % 2 === 0)){
         square7.innerHTML = "X"
-        board[2].splice(0, 0, "X")
-    }else{
+        board[2].splice(0, 1, "X")
+    } else {
         square7.innerHTML = "O"
-        board[2].splice(0, 0, "O")
+        board[2].splice(0, 1, "O")
     }
     console.log(board)
+    checkRowWin()
 })
 
 var square8 = document.getElementById("square8")
@@ -95,11 +100,12 @@ square8.addEventListener("click", function(el){
     if((turnMove() % 2 === 0)){
         square8.innerHTML = "X"
         board[2].splice(1, 1, "X")
-    }else{
+    } else {
         square8.innerHTML = "O"
         board[2].splice(1, 1, "O")
     }
     console.log(board)
+    checkRowWin()
 })
 
 var square9 = document.getElementById("square9")
@@ -107,29 +113,79 @@ square9.addEventListener("click", function(el){
     if((turnMove() % 2 === 0)){
         square9.innerHTML = "X"
         board[2].splice(2, 2, "X")
-    } else{
+    } else {
         square9.innerHTML = "O"
         board[2].splice(2, 2, "O")
     }
     console.log(board)
+    checkRowWin()
 })
 
+let turns = 0;
+
+// const turnMove = () => turns++;
+
+function turnMove() {
+    return turns++
+}
+
+function checkRowWin() {
+    board.forEach((row, index) => {
+        if (row.toString() === "X,X,X") {
+            if (index === 0) {
+                square1.style.backgroundColor = "yellow"
+                square2.style.backgroundColor = "yellow"
+                square3.style.backgroundColor = "yellow"
+            } else if (index === 1) {
+                square4.style.backgroundColor = "yellow"
+                square5.style.backgroundColor = "yellow"
+                square6.style.backgroundColor = "yellow"
+            } else {
+                square7.style.backgroundColor = "yellow"
+                square8.style.backgroundColor = "yellow"
+                square9.style.backgroundColor = "yellow"
+            }
+        } if (row.toString() === "O,O,O"){
+            if (index === 0) {
+                square1.style.backgroundColor = "yellow"
+                square2.style.backgroundColor = "yellow"
+                square3.style.backgroundColor = "yellow"
+            } else if (index === 1) {
+                square4.style.backgroundColor = "yellow"
+                square5.style.backgroundColor = "yellow"
+                square6.style.backgroundColor = "yellow"
+            } else {
+                square7.style.backgroundColor = "yellow"
+                square8.style.backgroundColor = "yellow"
+                square9.style.backgroundColor = "yellow"
+            }
+        }
+    })
+}
+
+// function checkWin() {
+//     board.forEach(function(element) {
+//         if (element === ["O", "O" ,"O"]) {
+//             console.log("O Wins")
+//         } else if (element === ["X", "X" ,"X"]) {
+//             console.log("X Wins")
+//         } else {
+//             console.log("Continue Playing!")
+//         }
+//     })
+// }
+
+// board.forEach(function(row) {
+//     if (row === ["O", "O" ,"O"]){
+//         console.log("O Wins!")
+//     } else if (row === ["X", "X" ,"X"]) {
+//         console.log("X Wins!")
+//     } else {
+//         console.log("Continue playing!")
+//     }
+// })
 
 
-board.forEach(function(row) {
-    if (row === ["O", "O" ,"O"]){
-        console.log("O Wins!")
-    } else if (row === ["X", "X" ,"X"]) {
-        console.log("X Wins!")
-    } else {
-        console.log("Continue playing!")
-    }
-})
-
-let turns = 0
-
-const turnMove = function() { 
-    turns++ }
 
 // let board = [
 //     ["", "", ""],
